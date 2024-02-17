9182dbb91b90a24e0c9e81d3ae865c15536a8c90
sam <156315526+samisuser@users.noreply.github.com>
2024-02-18 00:20:44 +0500

    cool

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
 var data = [
     {
         id: 1,
-        image1:"./assets/images/forest.png",
+        image1: "./assets/images/forest.png",
         area: "woods",
-        name1:"Photography"
+        name1: "Photography"
     },
     {
         id: 2,
-        image1:"./assets/images/tree.png",
+        image1: "./assets/images/sunforest.png",
         area: "path",
-        name1:"Website • Photography"
+        name1: "Website • Photography"
     },
     {
         id: 3,
-        image1:"./assets/images/desert.png",
+        image1: "./assets/images/desert.png",
         area: "darkness",
-        name1:"Website"
+        name1: "Website"
     },
     {
         id: 4,
-        image1:"./assets/images/ocean.png",
+        image1: "./assets/images/ocean.png",
         area: "waves",
-        name1:"Photography"
+        name1: "Photography"
     },
     {
         id: 5,
-        image1:"./assets/images/rain.png",
+        image1: "./assets/images/rain.png",
         area: "drops",
-        name1:"Photography"
+        name1: "Photography"
     },
     {
         id: 6,
-        image1:"./assets/images/kayaking.png",
+        image1: "./assets/images/kayaking.png",
         area: "ocean",
-        name1:"Website"
+        name1: "Website"
     },
     {
         id: 7,
-        image1:"./assets/images/town.png",
+        image1: "./assets/images/town.png",
         area: "town",
-        name1:"Website"
+        name1: "Website"
     },
     {
         id: 8,
-        image1:"./assets/images/mountain.png",
+        image1: "./assets/images/mountain.png",
         area: "mountain",
-        name1:"Website"
+        name1: "Website"
     },
     {
         id: 9,
-        image1:"./assets/images/camera.png",
+        image1: "./assets/images/camera.png",
         area: "camera",
-        name1:"Website"
+        name1: "Website"
     },
 ]
 
@@ -63,18 +63,84 @@ for (let i = 0; i < data.length; i++) {
 
     var card = document.createElement("div")
     card.className = "dark__card"
-    
+
     var area = document.createElement("h3")
     var name1 = document.createElement("p")
-    image1 = document.createAttribute("img")
+    picture = document.createElement("img")
+
+    area.className = "dark__card--h3"
+    name1.className = "dark__card--p"
+    picture.className = "dark__card--image"
+    card.className = "dark__card"
 
     area.textContent = (element.area)
     name1.textContent = (element.name1)
-    image1.setAttribute("src", element.image1)
-    
-    card.appendChild(image1)
+    picture.setAttribute("src", element.image1)
+
+    card.appendChild(picture)
     card.appendChild(area)
-    card.appendChild(image1)
+    card.appendChild(name1)
 
     dark.appendChild(card)
+}
+
+var member = [
+    {
+        cartoon: "./assets/images/man.png",
+        nick: "Ron Swanson",
+        level: "Founder",
+        social: "./assets/images/maninsocial.png "
+    },
+    {
+        cartoon: "./assets/images/woman.png",
+        nick: "Julia Depish",
+        level: "Marketing",
+        social: "./assets/images/maninsocial.png "
+    },
+    {
+        cartoon: "./assets/images/glass.png",
+        nick: "Danny Devry",
+        level: "Designer",
+        social: "./assets/images/glassinsocial.png "
+    },
+    {
+        cartoon: "./assets/images/japan.png",
+        nick: "Lisa Smith",
+        level: "Designer",
+        social: "./assets/images/japaninsocial.png "
+    }
+
+]
+
+var member = document.querySelector("#member")
+
+for (let i = 0; i < member.length; i++) {
+    const elem = member[i];
+    console.log(elem);
+
+    var card = document.createElement("div")
+    card.className = "member__card"
+
+    var nick = document.createElement("h3")
+    var level = document.createElement("p")
+    var cartoon = document.createElement("img")
+    var social = document.createElement("img")
+
+    nick.className = "member__card--h3"
+    level.className = "member__card--p"
+    cartoon.className = "member__card--image1"
+    social.className = "member__card--img"
+
+
+    nick.textContent = (element.nick)
+    level.textContent = (element.level)
+    cartoon.setAttribute("src", element.cartoon)
+    social.setAttribute("src", element.social)
+
+    card.appendChild(cartoon)
+    card.appendChild(nick)
+    card.appendChild(level)
+    card.appendChild(social)
+
+    member.appendChild(card)
 }
